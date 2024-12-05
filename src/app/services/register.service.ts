935f1dbf63159fda762aade96cc195e3b15903f4
Tommy Ngo <tommy.ngo@check24.de>
2024-12-05 12:14:59 +0100

    chore: create axios object

--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@angular/core';
-import axios from 'axios';
+import api from '../utils/api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
-  private baseUrl = 'http://localhost:5005';
   constructor() {}
 
   public async register(email: string, password: string): Promise<any> {
     try {
       // TODO: Richtigen Endpunkt nutzen
-      return (await axios.post(`${this.baseUrl}/api/data`, {email, password})).data;
+      return (await api.post(`/api/data`, {email, password})).data;
     } catch (error) {
       console.error('Registrierungsfehler:', error);
       throw error;

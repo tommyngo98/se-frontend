9927b35915c0b469b55adf643e0399ee32fdb95b
Tommy Ngo <tommy.ngo@check24.de>
2024-12-05 10:10:23 +0100

    chore: add input form for register

--- src/app/presentation/pages/register/register.component.ts
+++ src/app/presentation/pages/register/register.component.ts
@@ -1,23 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import { RegisterService } from "../../../services/register.service";
+import { InputFormComponent } from "../../ui-components/input-form/input-form.component";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [
-    RouterLink
-  ],
+  imports: [RouterLink, InputFormComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
-  constructor(private registerService: RegisterService) {
-  }
+  public constructor(private registerService: RegisterService) {}
 
-  // TODO: Argumente vom HTML Template nehmen
-  public register() {
-    this.registerService.register('test@test.de', 'test123')
+  public register($event: { email: string; password: string }): void {
+    this.registerService.register($event.email, $event.password)
       .then(response => {
         console.log(response);
       })
--- src/app/presentation/ui-components/input-form/input-form.component.ts
+++ src/app/presentation/ui-components/input-form/input-form.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, input, Output} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {CommonModule} from "@angular/common";
 
 @Component({
-  selector: 'app-input-form',
-  standalone: true,
-  imports: [],
+  selector: 'input-form',
   templateUrl: './input-form.component.html',
-  styleUrl: './input-form.component.scss'
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  styleUrls: ['./input-form.component.scss']
 })
 export class InputFormComponent {
+  public form: FormGroup;
+  public urlPath = input.required<string>();
+  public actionType = input.required<'Anmelden' | 'Registrieren'>();
 
+  @Output()
+  public formSubmit = new EventEmitter<{ email: string; password: string }>();
+
+  public constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get email() {
+    return this.form.get('email');
+  }
+
+  get password() {
+    return this.form.get('password');
+  }
+
+  public onSubmit(): void {
+    if (this.form.valid) {
+      this.formSubmit.emit(this.form.value);
+    }
+  }
 }

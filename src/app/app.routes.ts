157c7c654e27c36f644a45a5f6b8b380f9625dbf
Tommy Ngo <tommy.ngo@check24.de>
2024-12-09 18:03:38 +0100

    chore: add dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from "./presentation/pages/home/home.component";
-import {RegisterComponent} from "./presentation/pages/register/register.component";
-import {LoginComponent} from "./presentation/pages/login/login.component";
+import { HomeComponent } from "./presentation/pages/home/home.component";
+import { RegisterComponent } from "./presentation/pages/register/register.component";
+import { LoginComponent } from "./presentation/pages/login/login.component";
+import { DashboardComponent } from "./presentation/pages/dashboard/dashboard.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];

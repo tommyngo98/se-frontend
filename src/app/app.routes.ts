7b2c94fcded8544a2a95d20d10e0f6b73d7dba74
Tommy Ngo <tommy.ngo@check24.de>
2024-12-09 18:35:32 +0100

    chore: add auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { HomeComponent } from "./presentation/pages/home/home.component";
 import { RegisterComponent } from "./presentation/pages/register/register.component";
 import { LoginComponent } from "./presentation/pages/login/login.component";
 import { DashboardComponent } from "./presentation/pages/dashboard/dashboard.component";
+import { AuthGuard } from "./route-guards/auth.route-guard";
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+  },
 ];
--- src/app/presentation/pages/login/login.component.ts
+++ src/app/presentation/pages/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import {RouterLink} from "@angular/router";
-import {InputFormComponent} from "../../ui-components/input-form/input-form.component";
-import {LoginService} from "../../../services/login.service";
+import { Router, RouterLink } from "@angular/router";
+import { InputFormComponent } from "../../ui-components/input-form/input-form.component";
+import { LoginService } from "../../../services/login.service";
 
 @Component({
   selector: 'app-login',
@@ -14,12 +14,12 @@ import {LoginService} from "../../../services/login.service";
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  constructor(private loginService: LoginService) {}
+  constructor(private loginService: LoginService, private router: Router) {} // Inject Router
 
   login($event: { name: string, email: string; password: string }): void {
     this.loginService.login($event.email, $event.password)
-      .then(response => {
-        console.log(response);
+      .then(() => {
+        this.router.navigate(['/dashboard']);
       })
       .catch(error => {
         console.log(error);

15bb9ba7ea847b33f58b6770d167764be84200dc
Tommy Ngo <tommy.ngo@check24.de>
2024-12-14 17:53:15 +0100

    route to /error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from "./presentation/pages/login/login.component";
 import { DashboardComponent } from "./presentation/pages/dashboard/dashboard.component";
 import { DashboardRouteGuard } from "./route-guards/dashboard.route-guard";
 import { LoginRouteGuard } from "./route-guards/login.route-guard";
-import {ErrorComponent} from "./presentation/pages/error/error.component";
+import { ErrorComponent } from "./presentation/pages/error/error.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -19,5 +19,6 @@ export const routes: Routes = [
     component: DashboardComponent,
     canActivate: [DashboardRouteGuard],
   },
-  { path: '**', component: ErrorComponent }
+  { path: 'error', component: ErrorComponent },
+  { path: '**', redirectTo: '/error', pathMatch: 'full' },
 ];

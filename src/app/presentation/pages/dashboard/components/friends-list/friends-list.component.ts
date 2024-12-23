eff549a79d56450787724700b860b33491f68757
Tommy Ngo <tommy.ngo@check24.de>
2024-12-23 18:16:53 +0100

    add friends list

--- src/app/presentation/pages/dashboard/components/friends-list/friends-list.component.ts
+++ src/app/presentation/pages/dashboard/components/friends-list/friends-list.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { UserService } from "../../../../../services/user.service";
+import { User } from "../../../../../data-domain/models/user.model";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'friends-list',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './friends-list.component.html',
   styleUrl: './friends-list.component.scss'
 })
 export class FriendsListComponent {
-  constructor() {
+  public userId: string = '';
+  public searchResult: User | undefined;
+
+  constructor(private userService: UserService) {}
+
+  public async setSearchResult(userId: string): Promise<void> {
+    this.searchResult = await this.userService.getUserById(userId)
   }
 }

3d4f065fde74f6bcc32defdefc101a90bea82a79
Tommy Ngo <tommy.ngo@check24.de>
2025-01-11 19:08:46 +0100

    Update chat name

--- src/app/presentation/pages/dashboard/components/chat/chat.component.ts
+++ src/app/presentation/pages/dashboard/components/chat/chat.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, input } from '@angular/core';
 import { SocketService } from '../../../../../services/socket.service';
 import { FormsModule } from "@angular/forms";
+import { User } from "../../../../../data-domain/models/user.model";
+import { SelectFriendService } from "../../../../../services/select-friend.service";
 
 @Component({
   selector: 'app-chat',
@@ -10,14 +12,21 @@ import { FormsModule } from "@angular/forms";
   standalone: true
 })
 export class ChatComponent implements OnInit {
+  public selectedfriend: User | undefined;
   public chatId = input.required<string>();
   public senderId = input.required<number>();
   public message = '';
   public messages: { sender: string; text: string }[] = [];
 
-  public constructor(private socketService: SocketService) {}
+  public constructor(
+    private socketService: SocketService,
+    private selectFriendService: SelectFriendService) {}
 
   public ngOnInit(): void {
+    this.selectFriendService.selectedFriend.subscribe(
+      (friend) => (this.selectedfriend = friend)
+    );
+
     this.socketService.joinRoom(this.chatId());
 
     this.socketService.onNewMessage().subscribe((message) => {
--- src/app/presentation/pages/dashboard/components/friends-list/friends-list.component.ts
+++ src/app/presentation/pages/dashboard/components/friends-list/friends-list.component.ts
@@ -1,16 +1,28 @@
 import { Component, input } from '@angular/core';
 import { User } from "../../../../../data-domain/models/user.model";
 import { FriendsTileComponent } from "../friends-tile/friends-tile.component";
+import { NgClass } from "@angular/common";
+import { SelectFriendService } from "../../../../../services/select-friend.service";
 
 @Component({
   selector: 'friends-list',
   standalone: true,
   imports: [
-    FriendsTileComponent
+    FriendsTileComponent,
+    NgClass
   ],
   templateUrl: './friends-list.component.html',
   styleUrl: './friends-list.component.scss'
 })
 export class FriendsListComponent {
+  public constructor(private selectFriendService: SelectFriendService) {}
+
   public friends = input.required<User[]>();
+
+  public selectedFriend: User | undefined;
+
+  public selectFriend(friend: User) {
+    this.selectedFriend = friend;
+    this.selectFriendService.selectFriend(friend);
+  }
 }

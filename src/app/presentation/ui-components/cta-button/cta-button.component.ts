66c5a804587ebe28de91b9dab952bea42d97e058
Tommy Ngo <tommy.ngo@check24.de>
2025-01-26 13:03:21 +0100

    Fix MC

--- src/app/presentation/ui-components/cta-button/cta-button.component.ts
+++ src/app/presentation/ui-components/cta-button/cta-button.component.ts
@@ -1,7 +1,8 @@
-import {Component, input} from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
     selector: 'cta-button',
+    standalone: true,
     imports: [],
     templateUrl: './cta-button.component.html',
     styleUrl: './cta-button.component.scss'
